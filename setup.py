65888c4f4f8a05f7fc18c28626c10228116bd9d1
okawo <okawo.198@gmail.com>
2019-07-16 15:46:40 +0300

    minor update

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+	name="rubypass",
+	version="0.1.3",
+	description="A package to extract video urls from 2 russian websites",
+	long_description=open('README.md').read(),
+	long_description_content_type="text/markdown",
+	author="okawo",
+	author_email="okawo.198@gmail.com",
+	url="https://github.com/okawo80085/russianWebBypass",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+
+	py_modules=['rubypass'],
+)
